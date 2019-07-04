756e0dea3bebc87ecc041920dd91ff6c6b4d8fc2
Hajera Zemy <hzemy@purdue.edu>
2019-07-05 01:58:25 +0530

    Connected to server

--- src/Login.js
+++ src/Login.js
@@ -14,23 +14,69 @@ type Props = {
     changePageHandler: string => any
 };
 export default class Login extends Component<Props> {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: ""
+        }
+    }
+
+    signupUser = () => {
+        const {username, password} = this.state;
         const {changePageHandler} = this.props;
+        fetch(`http://localhost:8080/User/signup?username=${username}&password=${password}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.error) {
+                    alert(data.error)
+                } else {
+                    changePageHandler("home");
+                }
+            })
+    };
+
+    loginUser = () => {
+        const {username, password} = this.state;
+        const {changePageHandler} = this.props;
+        fetch(`http://localhost:8080/User/signup?username=${username}&password=${password}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.error && data.error === "Account does not exist.") {
+                    this.signupUser();
+                } else if (data.error) {
+                    alert(data.error)
+                } else {
+                    changePageHandler("home");
+                }
+            })
+    };
+
+    setUsername = (username) => {
+        this.setState({username})
+    };
+
+    setPassword = (password) => {
+        this.setState({password})
+    };
+
+    render() {
+        const {username, password} = this.state;
         return (
             <View>
-
                 <Text style={styles.heading}>Welcome! </Text>
                 <View style={styles.spacingHigh}/>
                 <Text style={styles.fieldText}>Username: </Text>
-                <TextInput placeholder="Enter username:"/>
+                <TextInput placeholder="Enter username:" value={username} onChangeText={this.setUsername}/>
                 <View style={styles.spacingSmall}/>
                 <Text style={styles.fieldText}>Password: </Text>
-                <TextInput placeholder="Enter password:"/>
+                <TextInput placeholder="Enter password:" value={password} onChangeText={this.setPassword}/>
                 <View style={styles.spacingSmall}/>
                 <View style={styles.alignLeftView}>
-                    <Button title={title = "Login"} onPress={() => changePageHandler("home")}/>
+                    <Button title={"Login"} onPress={this.loginUser}/>
                 </View>
-
             </View>
         );
     }
